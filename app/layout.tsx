e6f42899641184cc3eff596a1aa07baf2b13927d
ZaneZHU <z18019061830@gmail.com>
2024-11-25 15:53:26 +0100

    frontend

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,53 +1,29 @@
-"use client";
-
+import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
-import { createContext, useEffect, useState } from "react";
-import { auth } from "@/lib/firebase";
-import { onAuthStateChanged, User } from "firebase/auth";
+import Head from "next/head";
 import "./globals.css";
 
 const inter = Montserrat({ subsets: ["latin"] });
 
-export const AuthContext = createContext<{ user: User | null, loading: boolean }>({
-  user: null,
-  loading: true
-});
+export const metadata: Metadata = {
+  title: "Overlay",
+  description: "Making software understand you.",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [user, setUser] = useState<User | null>(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    try {
-      const unsubscribe = onAuthStateChanged(auth, (user) => {
-        console.log('Auth state changed:', user);
-        setUser(user);
-        setLoading(false);
-      }, (error) => {
-        console.error('Auth error:', error);
-      });
-
-      return () => unsubscribe();
-    } catch (error) {
-      console.error('Firebase initialization error:', error);
-      setLoading(false);
-    }
-  }, []);
-
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AuthContext.Provider value={{ user, loading }}>
-          <main className="flex flex-col h-[100dvh] w-full bg-gray-100 py-10 px-5 md:px-10 lg:px-[450px]">
-            <div className="flex-1 p-1 w-full overflow-hidden bg-gradient-to-br from-[#EF953F] to-[#567CDD] rounded-[28px] shadow-md shadow-gray-200">
-              {children}
-            </div>
-          </main>
-        </AuthContext.Provider>
+        <main className="flex flex-col h-[100dvh] w-full bg-gray-100 py-10 px-5 md:px-10 lg:px-[450px]">
+          <div className="flex-1 p-1 w-full overflow-hidden bg-gradient-to-br from-[#EF953F] to-[#567CDD] rounded-[28px] shadow-md shadow-gray-200">
+            {children}
+          </div>
+          
+        </main>
       </body>
     </html>
   );
